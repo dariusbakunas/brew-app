008d19f6b8037b4e5691b0d589291430e5f7e1fc
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-11-25 20:56:19 -0500

    one more auth0 test

--- src/server/middleware/auth0.test.js
+++ src/server/middleware/auth0.test.js
@@ -12,7 +12,7 @@ describe('auth0 Strategy', () => {
     getApolloClient.mockClear();
   });
 
-  test('verify sets user if email matches existing users', async () => {
+  test('verify sets user if email matches existing user', async () => {
     const done = jest.fn();
     const mockClient = {
       query: jest.fn(),
@@ -27,7 +27,6 @@ describe('auth0 Strategy', () => {
     };
 
     mockClient.query.mockReturnValue(Promise.resolve({ data: { userByEmail: testUser } }));
-
     getApolloClient.mockReturnValue(mockClient);
 
     const profile = {
@@ -43,6 +42,57 @@ describe('auth0 Strategy', () => {
 
     expect(mockClient.query.mock.calls.length).toBe(1);
     expect(mockClient.query.mock.calls[0][0].variables).toEqual({ email: 'test@gmail.com' });
+    expect(done.mock.calls.length).toBe(1);
+    expect(done.mock.calls[0][0]).toBeNull();
+    expect(done.mock.calls[0][1]).toBe(testUser);
+  });
+
+  test('verify creates new user if email does not match', async () => {
+    const done = jest.fn();
+    const mockClient = {
+      query: jest.fn(),
+      mutate: jest.fn(),
+    };
+
+    const testUser = {
+      email: 'test@gmail.com',
+      firstName: 'Family Name',
+      lastName: 'Given Name',
+      id: 'TEST_ID',
+      username: 'testusername',
+    };
+
+    // getUserByEmail return null
+    mockClient.query.mockReturnValue(Promise.resolve({ data: { userByEmail: null } }));
+
+    // createUser should be called, return testUser as a result
+    mockClient.mutate.mockReturnValue(Promise.resolve({ data: { createUser: testUser } }));
+    getApolloClient.mockReturnValue(mockClient);
+
+    const profile = {
+      emails: [{ value: 'test@gmail.com' }],
+      name: {
+        familyName: 'Family Name',
+        givenName: 'Given Name',
+      },
+      user_id: 'Test User ID',
+    };
+
+    await verify('testAccessToken', null, {}, profile, done);
+
+    expect(mockClient.query.mock.calls.length).toBe(1);
+    expect(mockClient.query.mock.calls[0][0].variables).toEqual({ email: 'test@gmail.com' });
+    expect(mockClient.mutate.mock.calls.length).toBe(1);
+    expect(mockClient.mutate.mock.calls[0][0].variables).toEqual({
+      input: {
+        email: 'test@gmail.com',
+        firstName: 'Given Name',
+        lastName: 'Family Name',
+        status: 'NEW',
+        username: 'Test User ID',
+      },
+    });
+
     expect(done.mock.calls.length).toBe(1);
     expect(done.mock.calls[0][0]).toBeNull();
     expect(done.mock.calls[0][1]).toBe(testUser);
