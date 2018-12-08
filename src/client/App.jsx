a503e237309d16e6fbaaa1920a2ad04712f8f821
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-12-08 12:04:51 -0500

    move login part to App

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,16 +1,81 @@
 import React from 'react';
+import gql from 'graphql-tag';
 import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+import { Transition } from 'semantic-ui-react';
 import Learn from './pages/Learn';
 import Dashboard from './pages/Dashboard';
+import withServerContext from './HOC/withServerContext';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import { USER_STATUS } from '../contants';
+
+const GET_LOGIN_QUOTE = gql`
+  {
+    randomQuote {
+      text
+      author
+    }
+  }
+`;
 
 class App extends React.Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      email: PropTypes.string,
+      firstName: PropTypes.string,
+      lastName: PropTypes.string,
+      status: PropTypes.oneOf(Object.values(USER_STATUS)),
+    }),
+  };
+
   render() {
+    if (!this.props.user) {
+      return (
+        <Query query={GET_LOGIN_QUOTE}>
+          {
+            ({ loading, data, error }) => {
+              if (error) { return 'Error'; }
+
+              return (
+                <Transition visible={!loading} animation='scale' duration={500}>
+                  <Login quote={data.randomQuote}/>
+                </Transition>
+              );
+            }
+          }
+        </Query>
+      );
+    }
+
+    if (this.props.user.status === USER_STATUS.GUEST) {
+      const { firstName, lastName, email } = this.props.user;
+
+      return (
+        <SignUp email={email} firstName={firstName} lastName={lastName}/>
+      );
+    }
+
+    if (this.props.user.status === USER_STATUS.NEW) {
+      return (
+        <div>Looks like your account is not activated yet, comback later</div>
+      );
+    }
+
+    if (this.props.user.status === USER_STATUS.INACTIVE) {
+      return (
+        <div>Looks like your account was deactivated, comback later</div>
+      );
+    }
+
     return (
       <React.Fragment>
         <Route path='/' exact component={Dashboard}/>
+        <Route path='/learn' component={Learn}/>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withServerContext(App);
--- src/client/pages/Dashboard.jsx
+++ src/client/pages/Dashboard.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
-import { Transition } from 'semantic-ui-react';
 import withServerContext from '../HOC/withServerContext';
-import Login from './Login';
-
-const GET_LOGIN_QUOTE = gql`
-  {
-    randomQuote {
-      text
-      author
-    }
-  }
-`;
 
 class Dashboard extends React.Component {
   static propTypes = {
@@ -26,24 +13,6 @@ class Dashboard extends React.Component {
   };
 
   render() {
-    if (!this.props.user) {
-      return (
-        <Query query={GET_LOGIN_QUOTE}>
-          {
-            ({ loading, data, error }) => {
-              if (error) { return 'Error'; }
-
-              return (
-                <Transition visible={!loading} animation='scale' duration={500}>
-                  <Login quote={data.randomQuote}/>
-                </Transition>
-              );
-            }
-          }
-        </Query>
-      );
-    }
-
     return (
       <div>Dashboard</div>
     );
