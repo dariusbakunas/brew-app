bb774e2e754a48800414e6100a9584eacf53ef46
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-12-08 09:58:05 -0500

    add router

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Learn from './pages/Learn';
+import Dashboard from './pages/Dashboard';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        TEST
-      </div>
+      <React.Fragment>
+        <Route path='/' exact component={Dashboard}/>
+      </React.Fragment>
     );
   }
 }
