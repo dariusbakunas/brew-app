9c466dd35ba48c3387f2d97d89b71865c7eaed64
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-12-11 21:03:22 -0500

    uikit Card component

---
+++ src/client/components/Card/Card.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+function Card(props) {
+  const {
+    children, hover, title, variation, className,
+  } = props;
+
+  const classes = classNames(
+    className,
+    'uk-card',
+    { [`uk-card-${variation}`]: variation },
+    'uk-card-body',
+    { 'uk-card-hover': hover },
+  );
+
+  return (
+    <div className={classes}>
+      {
+        title
+        && <h3 className="uk-card-title">{title}</h3>
+      }
+      {children}
+    </div>
+  );
+}
+
+Card.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  hover: PropTypes.bool,
+  title: PropTypes.string,
+  variation: PropTypes.oneOf(['default', 'primary', 'secondary']),
+};
+
+export default Card;
---
+++ src/client/components/Card/Card.md
@@ -0,0 +1,7 @@
+A basic card
+
+```js
+<Card title='Default'>
+  <p>Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</Card>
+```
