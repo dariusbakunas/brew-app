86d1749c525391761c86a524b9a1a4ccffd1f788
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-12-16 16:36:13 -0500

    support onclick/isActive for nav items

--- src/client/components/Nav/Item.jsx
+++ src/client/components/Nav/Item.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import { Link, matchPath, withRouter } from 'react-router-dom';
 
 function Item(props) {
-  const { as, label, url } = props;
+  const {
+    as, className, label, location, url,
+  } = props;
+
+  const isActive = !!matchPath(location.pathname, { path: url });
+
+  const classes = classNames(
+    className,
+    { 'uk-active': isActive },
+  );
 
   if (as === 'a') {
     return (
-      <li><a href={url}>{label}</a></li>
-    );
-  } else {
-    return (
-      <li><Link to={url}>{label}</Link></li>
+      <li><a href={url} onClick={props.onClick}>{label}</a></li>
     );
   }
+  return (
+      <li className={classes}><Link to={url} onClick={props.onClick}>{label}</Link></li>
+  );
 }
 
 Item.defaultProps = {
@@ -22,8 +31,13 @@ Item.defaultProps = {
 
 Item.propTypes = {
   as: PropTypes.oneOf(['a', 'link']),
+  className: PropTypes.string,
   label: PropTypes.string.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+  }),
+  onClick: PropTypes.func,
   url: PropTypes.string,
 };
 
-export default Item;
+export default withRouter(Item);
