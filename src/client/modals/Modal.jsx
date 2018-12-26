8c4086bd5aed9b009d8ec937f23f3d03b618236f
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-12-26 13:45:40 -0500

    use portal to avoid modal element being moved by uikit

--- src/client/modals/Modal.jsx
+++ src/client/modals/Modal.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import LoadingBar from '../components/LoadingBar';
 import Header from '../components/Header';
 
@@ -16,6 +18,9 @@ class Modal extends React.Component {
   constructor(props) {
     super(props);
     this.ref = React.createRef();
+    this.state = {
+      open: false,
+    };
   }
 
   handleHide = () => {
@@ -41,20 +46,26 @@ class Modal extends React.Component {
       id, error, header, loading,
     } = this.props;
 
-    return (
-      <div id={id} data-uk-modal ref={this.ref}>
-        <div className='uk-modal-dialog uk-modal-body'>
-          <button className='uk-modal-close-default' type='button' data-uk-close/>
-          <Header as='h2' className='uk-modal-title'>{header}</Header>
-          <LoadingBar active={loading}/>
-          {
-            error &&
-            <span className='uk-text-danger'>{this.state.error}</span>
-          }
-          {this.props.render(this.close)}
-        </div>
-      </div>
-    );
+    if (typeof document !== 'undefined') {
+      return ReactDOM.createPortal(
+        <div id={id} ref={this.ref} data-uk-modal>
+          <div className='uk-modal-dialog uk-modal-body'>
+            <button className='uk-modal-close-default' type='button' data-uk-close/>
+            <Header as='h2' className='uk-modal-title'>{header}</Header>
+            <LoadingBar active={loading}/>
+            {
+              error &&
+              <span className='uk-text-danger'>{this.state.error}</span>
+            }
+            {this.props.render(this.close)}
+          </div>
+        </div>,
+        document.body,
+      );
+    }
+
+    // no need to render modals server-side
+    return null;
   }
 }
 
