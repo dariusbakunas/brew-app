43863434de26515446be1592b25558e6201c8f96
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-12-26 13:46:24 -0500

    allow changing fieldset layout

--- src/client/components/Form/Fieldset.jsx
+++ src/client/components/Form/Fieldset.jsx
@@ -3,10 +3,11 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 function Fieldset(props) {
-  const { children, legend } = props;
+  const { children, layout, legend } = props;
 
   const classes = classNames(
     'uk-fieldset',
+    { [`uk-form-${layout}`]: layout },
   );
 
   return (
@@ -23,6 +24,7 @@ function Fieldset(props) {
 Fieldset.propTypes = {
   children: PropTypes.node,
   legend: PropTypes.string,
+  layout: PropTypes.oneOf(['stacked', 'horizontal']),
 };
 
 export default Fieldset;
