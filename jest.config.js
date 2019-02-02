66aac856bfb06922eb2b86da46b6332b9b733588
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-02-02 13:10:32 -0500

    fix coverage config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverageFrom: [
-    '**/*.{js,jsx}',
+    '**/*.{ts,tsx}',
     '!**/node_modules/**',
     '!**/vendor/**',
   ],
