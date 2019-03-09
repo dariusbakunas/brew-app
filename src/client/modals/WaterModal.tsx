17b1359ff9ff479506f2790863e9190ffb65a26b
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-03-09 12:08:04 -0500

    fix more linter errors

--- src/client/modals/WaterModal.tsx
+++ src/client/modals/WaterModal.tsx
@@ -1,58 +1,58 @@
 import * as React from 'react';
 import { compose, graphql } from 'react-apollo';
-import Modal from './Modal';
+import { Water, WaterInput } from '../../types';
 import { Button, Form } from '../components';
+import { InputChangeHandlerType } from '../components/Form/Input';
 import handleGrpahQLError from '../errors/handleGraphQLError';
 import {
   CREATE_WATER, UPDATE_WATER,
 } from '../queries';
-import { Water, WaterInput } from '../../types';
-import { InputChangeHandlerType } from '../components/Form/Input';
+import Modal from './Modal';
 
-type WaterModalProps = {
-  id: string,
-  createWater: (args: { variables: WaterInput }) => Promise<void>,
-  water: Water & { id: string },
-  onHide: () => void,
-  open: boolean,
-  refetchQuery: any,
-  updateWater: (args: { variables: WaterInput }) => Promise<void>,
-};
+interface IWaterModalProps {
+  id: string;
+  createWater: (args: { variables: WaterInput }) => Promise<void>;
+  water: Water & { id: string };
+  onHide: () => void;
+  open: boolean;
+  refetchQuery: any;
+  updateWater: (args: { variables: WaterInput }) => Promise<void>;
+}
 
 type WaterModalState = Water & {
   error?: string,
   loading: boolean,
   validationErrors: {
     [key: string]: string,
-  }
+  },
 };
 
-export class WaterModal extends React.Component<WaterModalProps> {
-  readonly state: Readonly<WaterModalState>;
-
-  static getDefaultState: () => WaterModalState = () => ({
-    name: null,
-    pH: null,
+export class WaterModal extends React.Component<IWaterModalProps> {
+  private static getDefaultState: () => WaterModalState = () => ({
     alkalinity: null,
-    calcium: null,
-    magnesium: null,
-    sodium: null,
-    sulfate: null,
-    chloride: null,
     bicarbonate: null,
+    calcium: null,
+    chloride: null,
     description: null,
     error: null,
     loading: false,
+    magnesium: null,
+    name: null,
+    pH: null,
+    sodium: null,
+    sulfate: null,
     validationErrors: null,
-  });
+  })
 
-  constructor(props: WaterModalProps) {
+  public readonly state: Readonly<WaterModalState>;
+
+  constructor(props: IWaterModalProps) {
     super(props);
     if (props.water) {
       this.state = {
         ...props.water,
-        loading: false,
         error: null,
+        loading: false,
         validationErrors: null,
       };
     } else {
@@ -60,28 +60,204 @@ export class WaterModal extends React.Component<WaterModalProps> {
     }
   }
 
-  componentDidUpdate(prevProps: WaterModalProps) {
+  public componentDidUpdate(prevProps: IWaterModalProps) {
     if (!prevProps.water && this.props.water) {
       this.setState({
         ...this.props.water,
-        loading: false,
         error: null,
+        loading: false,
         validationErrors: null,
       });
     }
   }
 
-  handleChange: InputChangeHandlerType = (e, { name, value }) => this.setState({ [name]: value });
+  public render() {
+    const {
+      water, id, open,
+    } = this.props;
 
-  handleHide = () => {
+    const {
+      error, validationErrors, name, loading, description, pH, alkalinity, calcium,
+      magnesium, sodium, chloride, sulfate, bicarbonate,
+    } = this.state;
+
+    return (
+      <Modal
+        error={error}
+        header={water ? water.name : 'New Water'}
+        id={id}
+        loading={loading}
+        onHide={this.handleHide}
+        open={open}
+        render={(close) => (
+          <Form onSubmit={(e) => this.handleSubmit(e, close)}>
+            <Form.Fieldset layout='stacked'>
+              <div className='uk-margin'>
+                <Form.Input
+                  disabled={loading}
+                  error={validationErrors ? validationErrors.name : null}
+                  label='Name'
+                  name='name'
+                  onChange={this.handleChange}
+                  value={name || ''}
+                  required={true}
+                />
+              </div>
+              <div className='uk-margin uk-grid-small uk-child-width-auto uk-grid'>
+                <div>
+                  <Form.Input
+                    disabled={loading}
+                    min={1}
+                    max={14}
+                    width='small'
+                    type='number'
+                    name='pH'
+                    label='pH'
+                    onChange={this.handleChange}
+                    step={0.01}
+                    value={pH || ''}
+                    required={true}
+                  />
+                </div>
+                <div>
+                  <Form.Input
+                    disabled={loading}
+                    min={0}
+                    max={1000}
+                    width='small'
+                    type='number'
+                    name='alkalinity'
+                    label='Alkalinity (CaCO&#8323;)'
+                    onChange={this.handleChange}
+                    step={1}
+                    value={alkalinity || ''}
+                    required={true}
+                  />
+                </div>
+              </div>
+              <div className='uk-margin uk-grid-small uk-child-width-auto uk-grid'>
+                <div>
+                  <Form.Input
+                    disabled={loading}
+                    min={0}
+                    max={1000}
+                    width='small'
+                    type='number'
+                    name='calcium'
+                    label='Calcium (Ca)'
+                    onChange={this.handleChange}
+                    step={0.01}
+                    value={calcium || ''}
+                    required={true}
+                  />
+                </div>
+                <div>
+                  <Form.Input
+                    disabled={loading}
+                    min={0}
+                    max={1000}
+                    width='small'
+                    type='number'
+                    name='magnesium'
+                    label='Magnesium (Mg)'
+                    onChange={this.handleChange}
+                    step={0.01}
+                    value={magnesium || ''}
+                    required={true}
+                  />
+                </div>
+                <div>
+                  <Form.Input
+                    disabled={loading}
+                    min={0}
+                    max={1000}
+                    width='small'
+                    type='number'
+                    name='sodium'
+                    label='Sodium (Na)'
+                    onChange={this.handleChange}
+                    step={0.01}
+                    value={sodium || ''}
+                    required={true}
+                  />
+                </div>
+              </div>
+              <div className='uk-margin uk-grid-small uk-child-width-auto uk-grid'>
+                <div>
+                  <Form.Input
+                    disabled={loading}
+                    min={0}
+                    max={1000}
+                    width='small'
+                    type='number'
+                    name='sulfate'
+                    label='Sulfate (SO&#8323;&#8322;)'
+                    onChange={this.handleChange}
+                    step={0.01}
+                    value={sulfate || ''}
+                    required={true}
+                  />
+                </div>
+                <div>
+                  <Form.Input
+                    disabled={loading}
+                    min={0}
+                    max={1000}
+                    width='small'
+                    type='number'
+                    name='chloride'
+                    label='Chloride (Cl)'
+                    onChange={this.handleChange}
+                    step={0.01}
+                    value={chloride || ''}
+                    required={true}
+                  />
+                </div>
+                <div>
+                  <Form.Input
+                    disabled={loading}
+                    min={0}
+                    max={1000}
+                    width='small'
+                    type='number'
+                    name='bicarbonate'
+                    label='Bicarbonate (HCO&#8323;)'
+                    onChange={this.handleChange}
+                    step={0.01}
+                    value={bicarbonate || ''}
+                    required={true}
+                  />
+                </div>
+              </div>
+              <Form.TextArea
+                disabled={loading}
+                name='description'
+                label='Description'
+                onChange={this.handleChange}
+                value={description || ''}
+              />
+            </Form.Fieldset>
+            <div className='uk-text-right'>
+              <Button className='uk-modal-close uk-margin-small-right' disabled={loading}>Cancel</Button>
+              <Button variation='primary' type='submit' disabled={loading}>Submit</Button>
+            </div>
+          </Form>
+        )}
+      />
+    );
+  }
+
+  private handleChange: InputChangeHandlerType = (e, { name, value }) => this.setState({ [name]: value });
+
+  private handleHide = () => {
     if (this.props.onHide) {
       this.props.onHide();
     }
 
     this.setState(WaterModal.getDefaultState());
-  };
+  }
 
-  handleSubmit = (e: React.FormEvent<HTMLFormElement>, closeModal: () => void) => {
+  private handleSubmit = (e: React.FormEvent<HTMLFormElement>, closeModal: () => void) => {
     e.preventDefault();
 
     const { createWater, updateWater, water } = this.props;
@@ -94,16 +270,16 @@ export class WaterModal extends React.Component<WaterModalProps> {
 
       const variables: WaterInput = {
         input: {
+          alkalinity,
+          bicarbonate,
+          calcium,
+          chloride,
+          description,
+          magnesium,
           name,
           pH,
-          alkalinity,
-          calcium,
-          magnesium,
           sodium,
           sulfate,
-          chloride,
-          bicarbonate,
-          description,
         },
       };
 
@@ -127,196 +303,20 @@ export class WaterModal extends React.Component<WaterModalProps> {
         });
       });
     });
-  };
-
-  render() {
-    const {
-      water, id, open,
-    } = this.props;
-
-    const {
-      error, validationErrors, name, loading, description, pH, alkalinity, calcium,
-      magnesium, sodium, chloride, sulfate, bicarbonate,
-    } = this.state;
-
-    return (
-      <Modal
-        error={error}
-        header={water ? water.name : 'New Water'}
-        id={id}
-        loading={loading}
-        onHide={this.handleHide}
-        open={open}
-        render={close => (
-          <Form onSubmit={e => this.handleSubmit(e, close)}>
-            <Form.Fieldset layout='stacked'>
-              <div className='uk-margin'>
-                <Form.Input
-                  disabled={loading}
-                  error={validationErrors ? validationErrors.name : null}
-                  label='Name'
-                  name='name'
-                  onChange={this.handleChange}
-                  value={name || ''}
-                  required
-                />
-              </div>
-              <div className='uk-margin uk-grid-small uk-child-width-auto uk-grid'>
-                <div>
-                  <Form.Input
-                    disabled={loading}
-                    min={1}
-                    max={14}
-                    width='small'
-                    type='number'
-                    name='pH'
-                    label='pH'
-                    onChange={this.handleChange}
-                    step={0.01}
-                    value={pH || ''}
-                    required
-                  />
-                </div>
-                <div>
-                  <Form.Input
-                    disabled={loading}
-                    min={0}
-                    max={1000}
-                    width='small'
-                    type='number'
-                    name='alkalinity'
-                    label='Alkalinity (CaCO&#8323;)'
-                    onChange={this.handleChange}
-                    step={1}
-                    value={alkalinity || ''}
-                    required
-                  />
-                </div>
-              </div>
-              <div className='uk-margin uk-grid-small uk-child-width-auto uk-grid'>
-                <div>
-                  <Form.Input
-                    disabled={loading}
-                    min={0}
-                    max={1000}
-                    width='small'
-                    type='number'
-                    name='calcium'
-                    label='Calcium (Ca)'
-                    onChange={this.handleChange}
-                    step={0.01}
-                    value={calcium || ''}
-                    required
-                  />
-                </div>
-                <div>
-                  <Form.Input
-                    disabled={loading}
-                    min={0}
-                    max={1000}
-                    width='small'
-                    type='number'
-                    name='magnesium'
-                    label='Magnesium (Mg)'
-                    onChange={this.handleChange}
-                    step={0.01}
-                    value={magnesium || ''}
-                    required
-                  />
-                </div>
-                <div>
-                  <Form.Input
-                    disabled={loading}
-                    min={0}
-                    max={1000}
-                    width='small'
-                    type='number'
-                    name='sodium'
-                    label='Sodium (Na)'
-                    onChange={this.handleChange}
-                    step={0.01}
-                    value={sodium || ''}
-                    required
-                  />
-                </div>
-              </div>
-              <div className='uk-margin uk-grid-small uk-child-width-auto uk-grid'>
-                <div>
-                  <Form.Input
-                    disabled={loading}
-                    min={0}
-                    max={1000}
-                    width='small'
-                    type='number'
-                    name='sulfate'
-                    label='Sulfate (SO&#8323;&#8322;)'
-                    onChange={this.handleChange}
-                    step={0.01}
-                    value={sulfate || ''}
-                    required
-                  />
-                </div>
-                <div>
-                  <Form.Input
-                    disabled={loading}
-                    min={0}
-                    max={1000}
-                    width='small'
-                    type='number'
-                    name='chloride'
-                    label='Chloride (Cl)'
-                    onChange={this.handleChange}
-                    step={0.01}
-                    value={chloride || ''}
-                    required
-                  />
-                </div>
-                <div>
-                  <Form.Input
-                    disabled={loading}
-                    min={0}
-                    max={1000}
-                    width='small'
-                    type='number'
-                    name='bicarbonate'
-                    label='Bicarbonate (HCO&#8323;)'
-                    onChange={this.handleChange}
-                    step={0.01}
-                    value={bicarbonate || ''}
-                    required
-                  />
-                </div>
-              </div>
-              <Form.TextArea
-                disabled={loading}
-                name='description'
-                label='Description'
-                onChange={this.handleChange}
-                value={description || ''}
-              />
-            </Form.Fieldset>
-            <div className='uk-text-right'>
-              <Button className='uk-modal-close uk-margin-small-right' disabled={loading}>Cancel</Button>
-              <Button variation='primary' type='submit' disabled={loading}>Submit</Button>
-            </div>
-          </Form>
-        )}
-      />
-    );
   }
 }
 
 export default compose(
   graphql(CREATE_WATER, {
     name: 'createWater',
-    options: (props: WaterModalProps) => ({
+    options: (props: IWaterModalProps) => ({
       awaitRefetchQueries: true,
       refetchQueries: [props.refetchQuery],
     }),
   }),
   graphql(UPDATE_WATER, {
     name: 'updateWater',
-    options: (props: WaterModalProps) => ({
+    options: (props: IWaterModalProps) => ({
       awaitRefetchQueries: true,
       refetchQueries: [props.refetchQuery],
     }),
--- src/client/pages/UsersPage.tsx
+++ src/client/pages/UsersPage.tsx
@@ -1,56 +1,26 @@
-import * as React from 'react';
-import { compose, graphql, Mutation } from 'react-apollo';
 import { ApolloError } from 'apollo-client';
+import * as React from 'react';
+import { compose, graphql } from 'react-apollo';
+import { User, UserStatus } from '../../types';
 import {
-  Container, Table, Spinner, IconNav,
+  Container, IconNav, Spinner, Table,
 } from '../components';
 import handleGraphQLError from '../errors/handleGraphQLError';
 import withServerContext from '../HOC/withServerContext';
 import { GET_ALL_USERS, REMOVE_USER } from '../queries';
-import { User, UserStatus } from '../../types';
 
-type UsersPageProps = {
+interface IUsersPageProps {
   user: {
     id: string,
-  },
+  };
   data: {
     loading: boolean,
     users: User[],
-  },
-  removeUser: (args: { variables: { id: string } }) => Promise<void>,
-};
+  };
+  removeUser: (args: { variables: { id: string } }) => Promise<void>;
+}
 
-class UsersPage extends React.Component<UsersPageProps> {
-  handleRemove({ id, username }: Partial<User>) {
-    const options = {
-      labels: { ok: 'Yes', cancel: 'No' },
-    };
-
-    window.UIkit.modal.confirm(`Are you sure you want to remove user '${username}'?`, options).then(() => {
-      this.setState({ loading: true }, () => {
-        this.props.removeUser({ variables: { id } })
-          .then(() => {
-            this.setState({ loading: false });
-          }).catch((err) => {
-            this.setState({ loading: false }, () => {
-              UsersPage.handleError(err);
-            });
-          });
-      });
-    }, () => {});
-  }
-
-  private static handleError(error: ApolloError) {
-    const { errorMessage } = handleGraphQLError(error, false);
-
-    window.UIkit.notification({
-      message: errorMessage,
-      status: 'danger',
-      pos: 'top-right',
-      timeout: 5000,
-    });
-  }
-
+class UsersPage extends React.Component<IUsersPageProps> {
   private static getStatusString(status: UserStatus) {
     return {
       [UserStatus.ACTIVE]: 'active',
@@ -58,6 +28,17 @@ class UsersPage extends React.Component<UsersPageProps> {
       [UserStatus.INACTIVE]: 'inactive',
       [UserStatus.NEW]: 'new',
     }[status];
+  }
+
+  private static handleError(error: ApolloError) {
+    const { errorMessage } = handleGraphQLError(error, false);
+
+    window.UIkit.notification({
+      message: errorMessage,
+      pos: 'top-right',
+      status: 'danger',
+      timeout: 5000,
+    });
   }
 
   // TODO: replace Mutation with HOC for easier testing
@@ -103,6 +84,25 @@ class UsersPage extends React.Component<UsersPageProps> {
       </React.Fragment>
     );
   }
+
+  private handleRemove({ id, username }: Partial<User>) {
+    const options = {
+      labels: { ok: 'Yes', cancel: 'No' },
+    };
+
+    window.UIkit.modal.confirm(`Are you sure you want to remove user '${username}'?`, options).then(() => {
+      this.setState({ loading: true }, () => {
+        this.props.removeUser({ variables: { id } })
+          .then(() => {
+            this.setState({ loading: false });
+          }).catch((err) => {
+          this.setState({ loading: false }, () => {
+            UsersPage.handleError(err);
+          });
+        });
+      });
+    });
+  }
 }
 
 export default withServerContext(
@@ -115,7 +115,7 @@ export default withServerContext(
           const { users } = cache.readQuery({ query: GET_ALL_USERS });
           cache.writeQuery({
             data: {
-              invitations: users.filter((user: User) => user.id !== id),
+              users: users.filter((user: User) => user.id !== id),
             },
             query: GET_ALL_USERS,
           });
