2f3783dda3dc224d3095a35decd681d4d08ecd15
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-04-13 09:40:03 -0400

    move graphql to hoc for login page

--- src/client/pages/Login.tsx
+++ src/client/pages/Login.tsx
@@ -1,63 +1,44 @@
-import gql from 'graphql-tag';
 import React from 'react';
-import { Query } from 'react-apollo';
+import { compose } from 'react-apollo';
 import { Container, Header, Icon } from '../components';
-
-const GET_LOGIN_QUOTE = gql`
-  query GetRandomQuote {
-    randomQuote {
-      text
-      author
-    }
-  }
-`;
+import { getLoginQuote } from '../HOC/login'
 
 interface ILoginPageProps {
-  data: {
+  getLoginQuote: {
     randomQuote: {
       author: string,
-      test: string,
+      text: string,
     },
   };
 }
 
 class Login extends React.Component<ILoginPageProps> {
   public render() {
-    // TODO: convert query to HOC
+    const { randomQuote } = this.props.getLoginQuote;
 
     return (
-      <Query query={GET_LOGIN_QUOTE}>
-        {
-          ({ loading, data, error }) => {
-            if (error) {
-              console.log(error);
-              return 'Error';
-            }
-
-            return (
-              <div className='login-container'>
-                <Container size='small'>
-                  <Header as='h1' textAlign='center'>BREW BEER</Header>
-                  <div className='hop-logo'>
-                    <a href='/auth' aria-label='Login'>
-                      <Icon icon='logo' className='animated pulse infinite'/>
-                    </a>
-                  </div>
-                  <blockquote className='login-quote'>
-                    <p className='uk-margin-small-bottom'>
-                      <q>{data && data.randomQuote ? data.randomQuote.text : ''}</q>
-                    </p>
-                    <footer>
-                      <cite>{data && data.randomQuote ? data.randomQuote.author : ''}</cite>
-                    </footer>
-                  </blockquote>
-                </Container>
-              </div>
-            );
-          }}
-      </Query>
+      <div className='login-container'>
+        <Container size='small'>
+          <Header as='h1' textAlign='center'>BREW BEER</Header>
+          <div className='hop-logo'>
+            <a href='/auth' aria-label='Login'>
+              <Icon icon='logo' className='animated pulse infinite'/>
+            </a>
+          </div>
+          <blockquote className='login-quote'>
+            <p className='uk-margin-small-bottom'>
+              <q>{randomQuote ? randomQuote.text : ''}</q>
+            </p>
+            <footer>
+              <cite>{randomQuote ? randomQuote.author : ''}</cite>
+            </footer>
+          </blockquote>
+        </Container>
+      </div>
     );
   }
 }
 
-export default Login;
+export default compose(
+  getLoginQuote,
+)(Login);
