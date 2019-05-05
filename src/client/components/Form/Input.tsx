06d5cc09dd499be284326c7371e8fd366113dd5e
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-05-05 13:59:05 -0400

    debounce and additional input properties

--- src/client/components/Form/Input.tsx
+++ src/client/components/Form/Input.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames';
+import debounce from 'lodash.debounce';
 import React from 'react';
+import { randomId } from '../../utils/random';
 import Icon from '../Icon';
 
 export type InputChangeHandlerType = (
@@ -7,50 +9,50 @@ export type InputChangeHandlerType = (
   val: { name: string, value: string | string[] | number | boolean },
 ) => void;
 
-interface InputProps {
+interface IInputProps {
+  debounced: boolean;
   disabled?: boolean;
   error?: string;
+  id?: string;
   label?: string;
   min?: number;
   max?: number;
   name: string;
   icon?: string;
   iconWidth?: string;
+  onBlur?: () => void;
   onChange?: InputChangeHandlerType;
   required?: boolean;
   step?: number;
+  style?: object;
   type?: 'text' | 'number';
   value?: string | number;
   width?: 'large' | 'medium' | 'small' | 'xsmall';
 }
 
-class Input extends React.Component<InputProps> {
-  public static defaultProps: Partial<InputProps> = {
+class Input extends React.Component<IInputProps> {
+  public static defaultProps: Partial<IInputProps> = {
     iconWidth: '20px',
     step: 1,
     type: 'text',
   };
 
-  public handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      name, onChange, step, type,
-    } = this.props;
-    const { value } = e.currentTarget;
+  private inputId: string;
 
-    if (onChange) {
-      if (value !== '' && type === 'number' && typeof value === 'string') {
-        const num = step === 1 ? parseInt(value, 10) :
-          parseFloat(value);
-        onChange(e, { name, value: num });
-      } else {
-        onChange(e, { name, value });
-      }
+  constructor(props: IInputProps) {
+    super(props);
+
+    this.inputId = randomId();
+
+    if (props.debounced) {
+      this.changeCallback = debounce(this.changeCallback, 300);
     }
   }
 
   public render() {
     const {
-      disabled, error, label, min, max, icon, iconWidth, name, required, step, type, value, width,
+      disabled, error, id, label, min, max, icon, iconWidth,
+      name, required, step, type, value, width, style,
     } = this.props;
 
     const classes = classNames(
@@ -59,26 +61,26 @@ class Input extends React.Component<InputProps> {
       { [`uk-form-width-${width}`]: width },
     );
 
-    const id = `${name}-input`;
-
     const inputElement = (
       <input
-        id={id}
+        id={id || this.inputId}
         className={classes}
         max={max}
         min={min}
         name={name}
         onChange={this.handleChange}
         step={type === 'number' ? step : null}
+        onBlur={this.props.onBlur}
         type={type}
         value={value}
         disabled={disabled}
         required={required}
+        style={style}
       />);
 
     return (
       <React.Fragment>
-        {label && <label className='uk-form-label' htmlFor={id}>{label}</label>}
+        {label && <label className='uk-form-label' htmlFor={this.inputId}>{label}</label>}
         { icon ? <div className='uk-inline'>
           <span className='uk-form-icon uk-form-icon-flip uk-icon'>
             <Icon icon={icon} width={iconWidth}/>
@@ -88,6 +90,29 @@ class Input extends React.Component<InputProps> {
         {error && <span className='uk-text-danger'>{error}</span>}
       </React.Fragment>
     );
+  }
+
+  private changeCallback: InputChangeHandlerType = (e, value) => {
+    if (this.props.onChange) {
+      this.props.onChange(e, value);
+    }
+  }
+
+  private handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      name, onChange, step, type,
+    } = this.props;
+    const { value } = e.currentTarget;
+
+    if (onChange) {
+      if (value !== '' && type === 'number' && typeof value === 'string') {
+        const num = step === 1 ? parseInt(value, 10) :
+          parseFloat(value);
+        this.changeCallback(e, { name, value: num });
+      } else {
+        this.changeCallback(e, { name, value });
+      }
+    }
   }
 }
 
