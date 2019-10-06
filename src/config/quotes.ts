a28301af54ae7cc0c06e7e039ae76d22eb886eb3
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-10-06 11:25:41 -0400

    convert login page to material-ui

---
+++ src/config/quotes.ts
@@ -0,0 +1,50 @@
+const quotes = [
+  {
+    author: 'Bill Owen',
+    text:
+      'Give a man a beer, waste an hour. Teach a man to brew, and waste a lifetime!',
+  },
+  {
+    author: 'Arnold Schwarzenegger',
+    text: 'Milk is for babies. When you grow up you have to drink beer.',
+  },
+  {
+    author: 'Kinky Friedman',
+    text: 'Beauty is in the eye of the beer holder.',
+  },
+  {
+    author: 'Abraham Lincoln',
+    text:
+      'I am a firm believer in the people. If given the truth, they can be depended upon to meet any national crisis. The great point is to bring them the real facts, and beer.',
+  },
+  {
+    author: 'Dave Barry',
+    text:
+      'Without question, the greatest invention in the history of mankind is beer. Oh, I grant you the wheel was also a fine invention, but the wheel does not go nearly as well with pizza.',
+  },
+  {
+    author: 'Benjamin Franklin',
+    text: 'Beer is proof that God loves us and wants us to be happy.',
+  },
+  { author: 'Stephen King', text: 'I work until beer o’clock.' },
+  { author: 'Plato', text: 'He is a wise man who invented beer.' },
+  {
+    author: 'Stephen Wright',
+    text: '24 hours in a day, 24 beers in a case. Coincidence?',
+  },
+  {
+    author: 'William Shakespeare',
+    text: 'For a quart of Ale is a meal for a King.',
+  },
+  {
+    author: 'Henry Lawson',
+    text: 'Beer makes you feel the way you ought to feel without beer.',
+  },
+  { author: 'Washington Irving', text: 'They who drink beer will think beer.' },
+  {
+    author: 'Homer Simpson',
+    text: 'Ah, beer. The cause of and the solution to all of life’s problems.',
+  },
+];
+
+export default quotes;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 import { withRouter } from 'next/router';
-import { WithRouterProps } from 'next/dist/client/with-router';
+import {
+  ExcludeRouterProps,
+  WithRouterProps,
+} from 'next/dist/client/with-router';
+import { makeStyles, Theme } from '@material-ui/core';
+import { NextPage } from 'next';
+import HopIcon from '../icons/HopIcon';
+import quotes from '../config/quotes';
 
-const Login: React.FC<WithRouterProps> = ({ router }) => {
+const useStyles = makeStyles<Theme>(theme => ({
+  logo: {
+    width: '4em',
+    height: '4em',
+    fill: '#9FB03E',
+    stroke: '#9FB03E',
+  },
+  header: {
+    fontWeight: 300,
+  },
+  quote: {
+    maxWidth: '500px',
+    '& q': {
+      fontStyle: 'italic',
+      fontSize: '1.25rem',
+      fontWeight: 300,
+    },
+    '& cite': {
+      color: '#666',
+      fontStyle: 'italic',
+      fontWeight: 300,
+    },
+  },
+}));
+
+interface LoginProps extends WithRouterProps {
+  quote: { author: string; text: string };
+}
+
+const Login: NextPage<LoginProps, ExcludeRouterProps<LoginProps>> = ({
+  quote,
+  router,
+}) => {
+  const classes = useStyles({});
+
   const handleLoginClick = () => {
     router.push({
       pathname: '/auth/login',
@@ -20,13 +62,38 @@ const Login: React.FC<WithRouterProps> = ({ router }) => {
       justify="center"
       style={{ minHeight: '100vh' }}
     >
-      <Grid item xs={3}>
-        <Button variant="contained" color="primary" onClick={handleLoginClick}>
-          LOGIN
-        </Button>
+      <Grid item>
+        <Typography
+          variant="h3"
+          component="h2"
+          gutterBottom
+          className={classes.header}
+        >
+          BREW BEER
+        </Typography>
+      </Grid>
+      <Grid item>
+        <IconButton aria-label="login" onClick={handleLoginClick}>
+          <HopIcon className={classes.logo} />
+        </IconButton>
+      </Grid>
+      <Grid item>
+        {quote && (
+          <blockquote className={classes.quote}>
+            <q>{quote.text}</q>
+            <footer>
+              <cite>&mdash;{quote.author}</cite>
+            </footer>
+          </blockquote>
+        )}
       </Grid>
     </Grid>
   );
 };
 
-export default withRouter(Login);
+Login.getInitialProps = async ctx => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  return { quote: randomQuote };
+};
+
+export default withRouter<LoginProps>(Login);
